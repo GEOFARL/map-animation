962b241eaceae599ac32b45448e4852337d3cd85
Maxim <toper.one111@gmail.com>
2025-04-22 14:18:27 +0300

    feat: add airlines card

---
+++ src/components/TimeCard.tsx
@@ -0,0 +1,9 @@
+const TimeCard: React.FC = () => {
+  return (
+    <div className="flex justify-center backdrop-blur-[20px] heading-h2 text-animation-red absolute top-[672.5px] left-[36px] rounded-[8px] border-[0.5px] py-6 px-[16px] border-[#94949480] w-[358px] h-[82px]">
+      Airlines
+    </div>
+  );
+};
+
+export default TimeCard;
