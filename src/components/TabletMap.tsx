09888af66a7cd06a54b488f3e20f57f4465a78e6
Maxim <toper.one111@gmail.com>
2025-04-22 14:01:32 +0300

    feat: add map

---
+++ src/components/TabletMap.tsx
@@ -0,0 +1,8 @@
+const TabletMap: React.FC = () => {
+  return (
+    <div className="relative w-[744px] h-[1000px] bg-[url('/images/australia-map.png')] bg-no-repeat bg-[length:531%] bg-[position:43.5%_36%]">
+      <div className="absolute inset-0 bg-black/40"></div>
+    </div>
+  );
+};
+export default TabletMap;
